4bdfb5bd240dcdf8e43cdfb23caf0248ec80f12f
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-02-21 14:14:07 +0630

    feat(): logger added

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,25 +1,39 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+type Config struct {
+	Addr      string
+	StaticDir string
+}
+
 func main() {
+	cfg := new(Config)
+	flag.StringVar(&cfg.Addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.StaticDir, "static-dir", "./ui/static", "Path to static assets")
+	flag.Parse()
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(cfg.StaticDir))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on :4000")
+	infoLog.Printf("Starting server on %s\n", cfg.Addr)
 	errCh := make(chan error, 1)
 
 	// Start the server in a goroutine and check for errors
 	go func() {
-		err := http.ListenAndServe(":4000", mux)
+		err := http.ListenAndServe(cfg.Addr, mux)
 		if err != nil {
 			errCh <- err
 		}
@@ -27,5 +41,5 @@ func main() {
 
 	// Wait for the error channel to receive an error or close
 	err := <-errCh
-	log.Fatal(err) // Log and exit gracefully
+	errorLog.Fatal(err) // Log and exit gracefully
 }
