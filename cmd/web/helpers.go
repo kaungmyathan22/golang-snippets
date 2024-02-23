ea885f982a480b8b78dcacc0f0bede17f1b268ef
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-02-23 15:49:54 +0630

    feat(): erorr prevention logic added

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"runtime/debug"
@@ -21,17 +22,20 @@ func (app *application) notFound(w http.ResponseWriter) {
 }
 
 func (app *application) render(w http.ResponseWriter, r *http.Request, name string, td *templateData) {
-	// Retrieve the appropriate template set from the cache based on the page n
-	// (like 'home.page.tmpl'). If no entry exists in the cache with the
-	// provided name, call the serverError helper method that we made earlier.
+
 	ts, ok := app.templateCache[name]
 	if !ok {
 		app.serverError(w, fmt.Errorf("the template %s does not exist", name))
 		return
 	}
-	// Execute the template set, passing in any dynamic data.
-	err := ts.Execute(w, td)
+
+	buf := new(bytes.Buffer)
+
+	err := ts.Execute(buf, td)
 	if err != nil {
 		app.serverError(w, err)
+		return
 	}
+
+	buf.WriteTo(w)
 }
