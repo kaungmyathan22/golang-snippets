4bc745989c692c9cbc7abfbcf9756eb4c290e3ab
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-02-23 15:22:56 +0630

    feat(sinppets): insert sinppet method added

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -10,16 +10,28 @@ type SnippetModel struct {
 	DB *sql.DB
 }
 
-func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
-}
-
-// This will return a specific snippet based on its id.
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
 	return nil, nil
 }
 
-// This will return the 10 most recently created snippets.
 func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
 	return nil, nil
+}
+
+func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
+
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+    VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
