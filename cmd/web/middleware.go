b1c668773662a754f487a4af7a5732e769a9c5ab
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-03-09 13:30:42 +0630

    update(): log request middleware added

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -4,10 +4,22 @@ import (
 	"net/http"
 )
 
+// secureHeaders → servemux → application handler
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("X-XSS-Protection", "1; mode=block")
 		w.Header().Set("X-Frame-Options", "deny")
+		// Any code here will execute on the way down the chain.
+		next.ServeHTTP(w, r)
+		// Any code here will execute on the way back up the chain.
+	})
+}
+
+// logRequest ↔ secureHeaders ↔ servemux ↔ application handler
+func (app *application) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+
 		next.ServeHTTP(w, r)
 	})
 }
