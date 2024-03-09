155bac2d1bc08a9916da74b5ecf10ac36b101546
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-03-09 13:50:59 +0630

    feat(): create snippet handler func added

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -43,6 +43,10 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
+func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
+
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		w.Header().Set("Allow", "POST")
