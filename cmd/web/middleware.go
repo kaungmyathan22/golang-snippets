5f45deb48e5a30db581febe8e0f03e82710891fd
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-03-10 13:42:36 +0630

    feat(): require authentication middleware method added

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -7,16 +7,13 @@ import (
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Create a deferred function (which will always be run in the event
-		// of a panic as Go unwinds the stack).
+
 		defer func() {
-			// Use the builtin recover function to check if there has been a
-			// panic or not. If there has...
+
 			if err := recover(); err != nil {
-				// Set a "Connection: close" header on the response.
+
 				w.Header().Set("Connection", "close")
-				// Call the app.serverError helper method to return a 500
-				// Internal Server response.
+
 				app.serverError(w, fmt.Errorf("%s", err))
 			}
 		}()
@@ -25,22 +22,30 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 	})
 }
 
-// secureHeaders → servemux → application handler
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("X-XSS-Protection", "1; mode=block")
 		w.Header().Set("X-Frame-Options", "deny")
-		// Any code here will execute on the way down the chain.
+
 		next.ServeHTTP(w, r)
-		// Any code here will execute on the way back up the chain.
+
 	})
 }
 
-// logRequest ↔ secureHeaders ↔ servemux ↔ application handler
 func (app *application) logRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
 
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) requireAuthenticatedUser(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if app.authenticatedUser(r) == 0 {
+			http.Redirect(w, r, "/user/login", 302)
+			return
+		}
 		next.ServeHTTP(w, r)
 	})
 }
