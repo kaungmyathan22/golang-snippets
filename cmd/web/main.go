71c0374d050cecb5fd1dc50c6423f0d3dccbd93d
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-03-10 13:37:55 +0630

    feat(): user added to the app context

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -24,6 +24,7 @@ type application struct {
 	errorLog      *log.Logger
 	infoLog       *log.Logger
 	snippets      *mysql.SnippetModel
+	users         *mysql.UserModel
 	session       *sessions.Session
 	templateCache map[string]*template.Template
 }
@@ -66,6 +67,7 @@ func main() {
 		errorLog:      errorLog,
 		infoLog:       infoLog,
 		snippets:      &mysql.SnippetModel{DB: db},
+		users:         &mysql.UserModel{DB: db},
 		session:       session,
 		templateCache: templateCache,
 	}
